9c2b81821bdf2b4ab6717ed9bf7c5a56f161b078
Holger Dinkel <6j5fnb2xrg@snkmail.com>
2012-08-20 10:57:59 +0200

    move EPSINS example to exercise_7; prettify comments

--- exercises/exercise_7.sh
+++ exercises/exercise_7.sh
@@ -1,13 +1,29 @@
 cd exercise_7
 
+################################################################################
+# view the whole file EPSINS.txt, but scroll pagewise
+less EPSINS.txt
+################################################################################
+
+################################################################################
 # print only those lines of the file EPSINS.txt, that contain the word "RecName"
 grep "RecName" EPSINS.txt
+################################################################################
 
+################################################################################
+# view the whole file EPSINS.fasta, but scroll pagewise
+less EPSINS.fasta
+################################################################################
+
+################################################################################
 # print only those lines of the file EPSINS.fasta that contain the character ">" (FASTA header)
 grep ">" EPSINS.fasta
+################################################################################
 
+################################################################################
 # print only those lines of the file EPSINS.fasta that DO NOT contain the character ">" (FASTA sequence)
 grep -v ">" EPSINS.fasta
+################################################################################
 
 
 
