a09f688233bcb7f5f2fb73e0451934a0e62359a1
Holger Dinkel <6j5fnb2xrg@snkmail.com>
2014-02-11 20:05:05 +0100

    change color style file for code boxes (pygments)

--- sphinx_beginner/conf.py
+++ sphinx_beginner/conf.py
@@ -79,12 +79,12 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-pygments_style = 'colorful'
-#pygments_style = 'lsi.lsiClass'
+#pygments_style = 'sphinx'
+#pygments_style = 'colorful'
+pygments_style = 'lsi.lsiClassStyle'
 
 # The default language to highlight source code in. The default is 'python'. The value should be a valid Pygments lexer name, see Showing code examples for more details.
-highlight_language = 'bash'
+highlight_language = 'console'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
